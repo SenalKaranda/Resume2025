c0748a40ce01845d32af1055355cd581e5709f0e
Senal Karanda <senalkaranda@outlook.com>
2025-05-20 22:53:17 -0400

    Added Github Actions!

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,16 @@ import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
+import pages from '@astrojs/gh-pages';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://SenalKaranda.github.io/Resume2025/',
   integrations: [
     react(),
     tailwind({
       applyBaseStyles: false,
     }),
+    pages(),
   ],
 });
